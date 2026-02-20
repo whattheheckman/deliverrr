09238b6b912e075fc73b30a9bea92ff45c9abec8
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2026-02-20 02:24:56 -0800

    package manager stuff

--- Assets/Prefabs/Managers/PackageManager.cs
+++ Assets/Prefabs/Managers/PackageManager.cs
@@ -4,15 +4,28 @@ using System.Collections.Generic;
 
 public class PackageManager : MonoBehaviour
 {
-
-    [SerializeField] private TextMeshProUGUI packagesLeftText;
+    [Header("Packages")]
     [SerializeField] private GameObject packageInstance;
-    [SerializeField] private List<GameObject> spawnLocations;
+    [SerializeField] private Transform[] spawnLocations;
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI packagesLeftText;
+
+    private 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (Transform spawnLocation in spawnLocations)
+        {
+            if (spawnLocation != null)
+            {
+                continue;
+            }
+            else
+            {
+                Instantiate(packageInstance, spawnLocation.position, spawnLocation.rotation);
+            }
+        }
     }
 
     // Update is called once per frame
