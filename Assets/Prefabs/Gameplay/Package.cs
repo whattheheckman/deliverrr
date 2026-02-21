90b12d88b455505c9447254953396188e536186c
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2026-02-20 21:11:02 -0800

    make package data file

--- Assets/Prefabs/Gameplay/Package.cs
+++ Assets/Prefabs/Gameplay/Package.cs
@@ -3,7 +3,22 @@ using UnityEngine;
 public class Package : MonoBehaviour
 {
     private int packageId;
-    
+    private SpriteRenderer spriteRenderer;
+    private Collider2D myCollider;
+    private bool isPickupable = false;
+
+    void OnStart()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    public bool packageCanBePickedUp() {return isPickupable;}
+    public void setPackageCanBePickedUp(bool canBePickedUp){
+        isPickupable = canBePickedUp;
+        myCollider.enabled = isPickupable;
+        spriteRenderer.enabled = isPickupable;
+    }
 
     public void setPackageID(int incomingId)
     {
